aa0cc056f235be507dcc82e6d48655287ce72815
Dominik Dancs <dominik@dancs.sk>
2021-08-16 19:58:54 +0200

    Switched to version 2.4.60

--- run-profit-trailer.sh
+++ run-profit-trailer.sh
@@ -5,7 +5,7 @@ APP=/app
 PT="ProfitTrailer"
 
 PT_DIR=${APP}/${PT}
-PT_ZIP=${BASE}/${PT}-${PT_VERSION}.zip
+PT_ZIP=${BASE}/${PT}-2.4.60.zip
 PT_JAR=${PT_DIR}/${PT}.jar
 PT_START="java -Djava.net.preferIPv4Stack=true -Dsun.stdout.encoding=UTF-8 -Dio.netty.allocator.numDirectArenas=0 -Djdk.nio.maxCachedBufferSize=262144 -XX:+UseSerialGC -XX:+UseStringDeduplication -Xms64m -Xmx512m -XX:CompressedClassSpaceSize=300m -XX:MaxMetaspaceSize=128m -jar $PT_JAR"
 

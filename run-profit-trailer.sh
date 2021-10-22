d27a45dee66fb54891b35b2676f33a9fdc3c80c2
Dominik Dancs <dominik@dancs.sk>
2021-10-22 21:31:35 +0200

    Updated to version 2.5.2

--- run-profit-trailer.sh
+++ run-profit-trailer.sh
@@ -3,14 +3,14 @@
 APP=/app
 
 PT_DIR=${APP}/ProfitTrailer
-PT_ZIP=${APP}/ProfitTrailer-2.5.1.zip
+PT_ZIP=${APP}/ProfitTrailer-2.5.2.zip
 PT_JAR=${PT_DIR}/ProfitTrailer.jar
 PT_START="java -Djava.net.preferIPv4Stack=true -Dsun.stdout.encoding=UTF-8 -Dio.netty.allocator.numDirectArenas=0 -Djdk.nio.maxCachedBufferSize=262144 -XX:+UseSerialGC -XX:+UseStringDeduplication -Xms64m -Xmx512m -XX:CompressedClassSpaceSize=300m -XX:MaxMetaspaceSize=128m -jar $PT_JAR"
 
 [ -d "$PT_DIR" ] || mkdir -p "$PT_DIR" || {
    echo "Error: no $PT_DIR found and could not make it. Exiting."; exit -1;
 }
-unzip -joqd ${PT_DIR} ${PT_ZIP} ProfitTrailer-2.5.1/ProfitTrailer.jar || {
+unzip -joqd ${PT_DIR} ${PT_ZIP} ProfitTrailer-2.5.2/ProfitTrailer.jar || {
   echo "Error: no ProfitTrailer jar found. Exiting."; exit -1;
 }
 cd ${PT_DIR} || {
